6db5256c7e5b7cb5b594afbbb4860da408ec9be2
muskan2345 <muskan2105gupta@gmail.com>
2021-07-04 17:47:58 +0530

    User register done

--- Interiorshop/interiorshop-main/interiorshop-main/apps/vendor/admin.py
+++ Interiorshop/interiorshop-main/interiorshop-main/apps/vendor/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Vendor
+from .models import Vendor,Customer
 
 admin.site.register(Vendor)
+admin.site.register(Customer)
--- Interiorshop/interiorshop-main/interiorshop-main/apps/vendor/models.py
+++ Interiorshop/interiorshop-main/interiorshop-main/apps/vendor/models.py
@@ -24,3 +24,19 @@ class Vendor(models.Model):
     def get_paid_amount(self):
         items = self.items.filter(vendor_paid=True, order__vendors__in=[self.id])
         return sum((item.product.price * item.quantity) for item in items)
+
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True, default='null')
+    password = models.CharField(max_length=15)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.OneToOneField(User, related_name='customer', on_delete=models.CASCADE)
+    
+
+    USERNAME_FIELD='name'
+
+    class Meta:
+        ordering=['name']
+
+    def __str__(self):
+        return self.name    
