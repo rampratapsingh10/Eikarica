de6c6877b55827a628a0f732c4f7b770b56cdda8
medha <medhachaudhary2000@gmail.com>
2021-07-04 17:28:18 +0530

    Changes in contact page and added about.html

--- Interiorshop/interiorshop-main/interiorshop-main/apps/core/urls.py
+++ Interiorshop/interiorshop-main/interiorshop-main/apps/core/urls.py
@@ -13,5 +13,6 @@ from . import views
 
 urlpatterns = [
     path('', views.frontpage, name='frontpage'),
-    path('contact/', views.contact, name='contact')
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about')
 ]
